cd95e505a235feffda99f3c3361b001a18a2090c
Miguel Péres <foxmpc@gmail.com>
2018-01-23 14:58:31 -0200

    Add transfer related tests

--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -2,10 +2,17 @@ defmodule FinancialSystemTest do
   use ExUnit.Case
   doctest FinancialSystem
 
-  test "User should be able to transfer money to another account" do
-    source_account = %FinancialSystem.Account{balance: 10.0}
-    dest_account = %FinancialSystem.Account{balance: 0.0}
+  setup_all do
+    {
+      :ok,
+      [
+        account_1: %FinancialSystem.Account{balance: 10.0},
+        account_2: %FinancialSystem.Account{balance: 0.0}
+      ]
+    }
+  end
 
+  test "User should be able to transfer money to another account", %{account_1: source_account, account_2: dest_account} do
     expected_result = {
       %FinancialSystem.Account{balance: 3.0},
       %FinancialSystem.Account{balance: 7.0},
@@ -14,8 +21,16 @@ defmodule FinancialSystemTest do
     assert FinancialSystem.transfer!(source_account, dest_account, 7.0) == expected_result
   end
 
-  test "User cannot transfer if not enough money available on the account" do
-    assert :false
+  test "Check balance of an account that doesn't have enough money", %{account_2: account} do
+    assert FinancialSystem.has_enough(account, 10.0) == false
+  end
+
+  test "Check balance of an account that has enough money", %{account_1: account} do
+    assert FinancialSystem.has_enough(account, 10.0) == true
+  end
+
+  test "User cannot transfer if not enough money available on the account", %{account_1: source_account, account_2: dest_account} do
+    assert FinancialSystem.transfer(dest_account, source_account, 5.0) == {:error, "Not enough money. (balance: #{dest_account.balance})"}
   end
 
   test "A transfer should be cancelled if an error occurs" do
