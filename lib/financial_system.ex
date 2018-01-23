8cd226af3bec3ebe089eeba62df74a695faaa5ec
Miguel Péres <foxmpc@gmail.com>
2018-01-23 13:22:08 -0200

    Add basic transfer money function

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -3,4 +3,26 @@ defmodule FinancialSystem do
   Documentation for FinancialSystem.
   """
 
+  def transfer(source_account, destination_account, value) do
+    case source_account.balance >= value do
+      true ->
+        { 
+          :ok,
+          {
+            %FinancialSystem.Account{source_account | balance: source_account.balance - value}, 
+            %FinancialSystem.Account{destination_account | balance: destination_account.balance + value}
+          }
+        }
+      false ->
+        {:error, "Not enough money. (#{source_account.balance})"}
+    end
+  end
+
+  def transfer!(source_account, destination_account, value) do
+    case transfer(source_account, destination_account, value) do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
+  end
+
 end
--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -2,4 +2,35 @@ defmodule FinancialSystemTest do
   use ExUnit.Case
   doctest FinancialSystem
 
+  test "User should be able to transfer money to another account" do
+    source_account = %FinancialSystem.Account{balance: 10.0}
+    dest_account = %FinancialSystem.Account{balance: 0.0}
+
+    expected_result = {
+      %FinancialSystem.Account{balance: 3.0},
+      %FinancialSystem.Account{balance: 7.0},
+    }
+
+    assert FinancialSystem.transfer!(source_account, dest_account, 7.0) == expected_result
+  end
+
+  test "User cannot transfer if not enough money available on the account" do
+    assert :false
+  end
+
+  test "A transfer should be cancelled if an error occurs" do
+    assert :false
+  end
+
+  test "A transfer can be splitted between 2 or more accounts" do
+    assert :false
+  end
+
+  test "User should be able to exchange money between different currencies" do
+    assert :false
+  end
+
+  test "Currencies should be in compliance with ISO 4217" do
+    assert :false
+  end
 end
