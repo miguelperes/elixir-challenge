606d85873302cb8c1f51774f836563bc79eb69cb
Miguel Péres <foxmpc@gmail.com>
2018-01-25 13:01:12 -0200

    Refactor code to support Money struct
    Change occurrences of balance from float to Money, to avoid floating point arithetic issues.
    Add helper functions to facilitate operations (addition and subtraction) with the Money struct.

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -9,13 +9,13 @@ defmodule FinancialSystem do
         { 
           :ok,
           {
-            %FinancialSystem.Account{source_account | balance: source_account.balance - value}, 
-            %FinancialSystem.Account{destination_account | balance: destination_account.balance + value}
+            %FinancialSystem.Account{source_account | balance: FinancialSystem.sub(source_account.balance, value)},  
+            %FinancialSystem.Account{destination_account | balance: FinancialSystem.add(destination_account.balance, value)}
           }
         }
 
       false ->
-        {:error, "Not enough money. (balance: #{source_account.balance})"}
+        {:error, "Not enough money. (balance: #{source_account.balance.amount})"}
     end
   end
 
@@ -26,8 +26,28 @@ defmodule FinancialSystem do
     end
   end
 
-  def has_enough(%FinancialSystem.Account{balance: account_balance}, value) do
-    account_balance >= value
+  def add(money, value) when is_float(value) or is_integer(value) or is_binary(value) do
+    FinancialSystem.add(money, Decimal.new(value))
+  end
+
+  def add(%Money{amount: amount, currency: currency}, value) do
+    amount
+    |> Decimal.add(value)
+    |> Money.new(currency)
+  end
+
+  def sub(money, value) when is_float(value) or is_integer(value) do
+    FinancialSystem.sub(money, Decimal.new(value))
+  end
+
+  def sub(%Money{amount: amount, currency: currency}, value) do
+    amount
+    |> Decimal.sub(value)
+    |> Money.new(currency)
+  end
+
+  def has_enough(%FinancialSystem.Account{balance: money}, value) do
+    Decimal.to_float(money.amount) >= value
   end
 
 end
--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -6,19 +6,10 @@ defmodule FinancialSystemTest do
     {
       :ok,
       [
-        account_1: %FinancialSystem.Account{balance: 10.0},
-        account_2: %FinancialSystem.Account{balance: 0.0}
+        account_1: %FinancialSystem.Account{balance: Money.new(:BRL, "10.0")},
+        account_2: %FinancialSystem.Account{balance: Money.new(:BRL, "0.0")}
       ]
     }
-  end
-
-  test "User should be able to transfer money to another account", %{account_1: source_account, account_2: dest_account} do
-    expected_result = {
-      %FinancialSystem.Account{balance: 3.0},
-      %FinancialSystem.Account{balance: 7.0},
-    }
-
-    assert FinancialSystem.transfer!(source_account, dest_account, 7.0) == expected_result
   end
 
   test "Check balance of an account that doesn't have enough money", %{account_2: account} do
@@ -29,8 +20,40 @@ defmodule FinancialSystemTest do
     assert FinancialSystem.has_enough(account, 10.0) == true
   end
 
-  test "User cannot transfer if not enough money available on the account", %{account_1: source_account, account_2: dest_account} do
-    assert FinancialSystem.transfer(dest_account, source_account, 5.0) == {:error, "Not enough money. (balance: #{dest_account.balance})"}
+  test "Add some amount to a money struct", %{account_1: %FinancialSystem.Account{balance: money}} do
+    value_to_add = 1.75
+    
+    expected_result = 
+      value_to_add
+      |> Decimal.new
+      |> Decimal.add(money.amount)
+      |> Money.new(:BRL)
+
+    assert FinancialSystem.add(money, value_to_add) == expected_result
+  end
+
+  test "Subtract some amount from a money struct", %{account_1: %FinancialSystem.Account{balance: money}} do
+    value_to_sub = Decimal.new(1.75)
+    
+    expected_result = 
+      money.amount
+      |> Decimal.sub(value_to_sub)
+      |> Money.new(:BRL)
+
+    assert FinancialSystem.sub(money, value_to_sub) == expected_result
+  end
+
+  test "User should be able to transfer money to another account", %{account_1: source_account, account_2: dest_account} do
+    expected_result = {
+      %FinancialSystem.Account{balance: Money.new(:BRL, "3.0")},
+      %FinancialSystem.Account{balance: Money.new(:BRL, "7.0")},
+    }
+
+    assert FinancialSystem.transfer!(source_account, dest_account, 7.0) == expected_result
+  end
+
+  test "User cannot transfer if not enough money available on the account", %{account_1: dest_account, account_2: source_account} do
+    assert FinancialSystem.transfer(source_account, dest_account, 5.0) == {:error, "Not enough money. (balance: #{source_account.balance.amount})"}
   end
 
   test "A transfer should be cancelled if an error occurs" do
