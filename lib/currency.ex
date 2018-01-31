21e94f828cf5f43037c5ef077b5e5e062a27bef0
Miguel Péres <foxmpc@gmail.com>
2018-01-31 15:06:24 -0200

    Check if currency is valid before converting

--- lib/currency.ex
+++ lib/currency.ex
@@ -15,23 +15,53 @@ defmodule FinancialSystem.Currency do
       end)
   end
 
-  def convert(value, same_currency, same_currency), do: value
+  def convert(value, same_currency, same_currency) do
+    case valid_currency?(same_currency) do
+      true -> {:ok, value}
+      false -> {:error, "Currency (#{same_currency}) not compliant with ISO 4217"}
+    end
+  end
 
   def convert(value, :USD, to_currency) do
-    rates = FinancialSystem.Currency.parse_file("currency_rates.txt")
-    value * rates[to_currency] |> Float.round(2)
+    case valid_currency?(to_currency) do
+      true -> 
+        rates = FinancialSystem.Currency.parse_file("currency_rates.txt")
+        converted_value = value * rates[to_currency] |> Float.round(2)        
+        {:ok, converted_value}
+
+      false -> {:error, "Currency (#{to_currency}) not compliant with ISO 4217"}
+    end
   end
 
   def convert(value, from_currency, :USD) do
-    rates = FinancialSystem.Currency.parse_file("currency_rates.txt")
-    value / rates[from_currency] |> Float.round(2)
+    case valid_currency?(from_currency) do
+      true ->
+        rates = FinancialSystem.Currency.parse_file("currency_rates.txt")
+        converted_value = value / rates[from_currency] |> Float.round(2)
+        {:ok, converted_value}
+  
+      false -> {:error, "Currency (#{from_currency}) not compliant with ISO 4217"}
+    end
   end
 
   def convert(value, from_currency, to_currency) do
-    usd_value = FinancialSystem.Currency.convert(value, from_currency, :USD)
-    FinancialSystem.Currency.convert(usd_value, :USD, to_currency)
+    usd_value = FinancialSystem.Currency.convert!(value, from_currency, :USD)
+    FinancialSystem.Currency.convert!(usd_value, :USD, to_currency)
   end
 
   def convert(value, exchange_rate), do: Float.round(value * exchange_rate, 2)
+
+  def convert!(value, currency_a, currency_b) do
+    case convert(value, currency_a, currency_b) do
+      {:ok, result} -> result
+      {:error, reason} -> raise(reason)
+    end
+  end
+
+  def valid_currency?(currency_to_check) do
+    FinancialSystem.Currency.parse_file("currency_rates.txt")
+    |> Keyword.keys
+    |> Enum.any?(fn(currency) -> currency == currency_to_check end)
+  end
 
 end
