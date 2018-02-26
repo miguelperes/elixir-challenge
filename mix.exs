ae444becad362a4057e074d5b2d27fefd6cdc0d6
Miguel Péres <foxmpc@gmail.com>
2018-02-26 15:51:28 -0300

    Release v1.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FinancialSystem.Mixfile do
   def project do
     [
       app: :financial_system,
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
